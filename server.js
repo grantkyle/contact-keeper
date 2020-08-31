41190b847b6eaec3be08d83cfddada93b22ff870
Grant Kyle <sgrantkyle@gmail.com>
2020-08-31 18:40:39 -0500

    mongoDB connection woes

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...'}))
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ContactKeeper API..." })
+);
 
 // Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
