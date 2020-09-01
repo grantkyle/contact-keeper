b4e721adc334b4b1dda7808dec4785ea8e46e185
Grant Kyle <sgrantkyle@gmail.com>
2020-09-01 12:52:26 -0500

    express validator for name, email, password route checking

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User')
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post('/', (req, res) => {
-    res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add a name").not().isEmpty(),
+    check("email", "Please include a valid email").isLength({
+      min: 6,
+    }),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please include a password with 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('Passed all checks')
+  }
+);
 
 module.exports = router;
