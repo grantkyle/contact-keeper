0153f17d5d952aa1befcaa77a19d59b74cc6eb03
Grant Kyle <sgrantkyle@gmail.com>
2020-09-01 12:12:12 -0500

    atlas connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
+const connectDB = async () => {
     mongoose.connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true,
