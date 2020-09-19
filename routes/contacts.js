fa38aee2a320ba9399cfe1b289380967dc61418b
Grant Kyle <sgrantkyle@gmail.com>
2020-09-19 16:42:21 -0500

    put functionality foundations

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,7 +11,7 @@ const Contact = require("../models/Contact");
 router.get("/", auth, async (req, res) => {
   try {
     const contacts = await Contact.find({ user: req.user.id }).sort({
-      date: -1
+      date: -1,
     });
     res.json(contacts);
   } catch (err) {
@@ -25,8 +25,8 @@ router.get("/", auth, async (req, res) => {
 // @access Private
 router.post(
   "/",
-  [ auth, [check("name", "Name is required").not().isEmpty()]],
-  
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -48,7 +48,7 @@ router.post(
 
       res.json(contact);
     } catch (error) {
-        console.error(error.message);
+      console.error(error.message);
       res.status(500).send("server error!!!");
     }
   }
@@ -57,8 +57,37 @@ router.post(
 // @route PUT api/contacts/:id
 // @desc Update contact
 // @access Private
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //Contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // does user own contact?
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not Authorized!!!" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
 
 // @route DELETE api/contacts/:id
